78bc12a73dfdc73f3d1432dfbbcdb0526b906ede
borsosl <borsosl@gmail.com>
2020-06-27 12:20:30 +0200

    Convert common word entry format

--- script/sabd-to-yaml/index.ts
+++ script/sabd-to-yaml/index.ts
@@ -3,12 +3,14 @@ import * as fs from "fs";
 import {sabdToYaml} from './sabd-to-yaml';
 
 interface Options {
-    input: string
+    format: string;
+    input: string;
 }
 
 const opt: Options = cli([
+    { name: 'format', alias: 'f', type: String },   // vi|co = vinód|common
     { name: 'input', type: String, defaultOption: true }
 ]);
 
 const txt = fs.readFileSync(opt.input).toString();
-process.stdout.write(Buffer.from(sabdToYaml(txt)));
+process.stdout.write(Buffer.from(sabdToYaml(txt, opt.format)));
--- script/sabd-to-yaml/sabd-to-yaml.ts
+++ script/sabd-to-yaml/sabd-to-yaml.ts
@@ -1,5 +1,6 @@
 import {DictEntriesFile, Ford, Szavak} from '../common/schema';
 import * as YAML from 'yaml';
+import {processLine as devaToKtrans} from '../ktrans-to-unicode/process-unicode-text';
 
 const enum Section {
     SourceLang,
@@ -8,11 +9,20 @@ const enum Section {
 }
 
 const boundary = [' ', ',', ';', '(', ')'];
-const grammatical = ['hn', 'nn', 'szn', 'nv', 't', 'tl', 'nt', 'mn', '1sz', 'tbsz', 'kt', 'ht', 'nvált', 'elölj'];
+const hunGram = ['hn', 'nn', 'szn', 'nv', 't', 'tl', 'nt', 'mn', '1sz', 'tbsz', 'kt', 'ht', 'nvált', 'elölj'];
+// noinspection NonAsciiCharacters
+const engGram: {[key: string]: string} = {
+    m: 'hn', f: 'nn', tr: 't', it: 'tl', intr: 'tl',
+    adj: 'mn', adv: 'ht', ppos: 'nt', prep: 'elölj',
+    sing: '1sz', p: 'tbsz', pl: 'tbsz', i: 'nvált', inv: 'nvált',
+    v: '', inf: '', fml: '', 'közel': '', 'távol': ''};
 
 const lineSplitter = /\r?\n/;
 const uppercasedWord = /^[A-Z]/;
-const meaningWithComment = /(.*)\((.*)\)$/;
+const meaningWithComment = /(.*) \((.*)\)$/;
+const commonLineParts = /(([^;:=]*);)?([^:=]*)(:([^:=]*))?(:([^:=]*))?(:([^:=]*))?=?(.*)/;
+const commaSplitter = /\s*,\s*/;
+const nonAlphaSplitter = /\W+/;
 
 const entriesFile = {
     datum: new Date().toISOString().substr(0, 10),
@@ -25,25 +35,30 @@ const entriesFile = {
 
 let entry: Szavak;
 
-export function sabdToYaml(txt: string) {
+export function sabdToYaml(txt: string, fmt: string) {
     const lines = txt.split(lineSplitter);
-    for(const line of lines)
-        processLine(line);
+    for(let line of lines) {
+        line = devaToKtrans(line, false);
+        fmt === 'vi' ? processVinodLine(line) : processCommonLine(line);
+    }
     cleanupEntry();
     return YAML.stringify(entriesFile);
 }
 
-function processLine(line: string) {
-    const hindiEnd = line.lastIndexOf('÷');
+function processVinodLine(line: string) {
+    let hindiEnd = line.lastIndexOf('÷');
     if(hindiEnd === -1)
-        return;
+        hindiEnd = line.length;
     let hindi = line.substr(0, hindiEnd).trim().replace(/[÷×]/g, '');
-    let hun = line.substring(hindiEnd+1, line.length).trim().replace(/[÷×]/g, '');
+    if(!hindi)
+        return;
+    let hun = hindiEnd === line.length ? ''
+        : line.substring(hindiEnd+1, line.length).trim().replace(/[÷×]/g, '');
     let isExpr = false;
-    if(hindi[0] === '←') {
+    if(hindi[0] === '←' || hindi[0] === '<') {
         // source word with own entry
         hindi = hindi.substring(1).trim();
-    } if(hindi[0] === '→') {
+    } if(hindi[0] === '→' || hindi[0] === '>') {
         hindi = hindi.substring(1).trim();
         isExpr = true;
     }
@@ -81,7 +96,7 @@ function processLine(line: string) {
                     sec = Section.Grammar;
             }
             if(sec === Section.Grammar) {
-                if(grammatical.includes(word)) {
+                if(hunGram.includes(word)) {
                     if(ford.nyt)
                         ford.nyt += ' ';
                     ford.nyt += word;
@@ -123,7 +138,124 @@ function processLine(line: string) {
         p++;
     }
     entry.ford.push(ford);
-    // console.log(entry.szo);
+}
+
+function processCommonLine(line: string) {
+    const parts = commonLineParts.exec(line.replace(/[÷×]/g, ''));
+    let [,, title, hindi,, nyt,, szarm,, alt, hun] = parts;
+    if(!hindi)
+        return;
+    else
+        hindi = hindi.trim();
+    if(title)
+        title = title.trim();
+    if(nyt)
+        nyt = nyt.trim();
+    if(szarm)
+        szarm = szarm.trim();
+    if(hun)
+        hun = hun.trim();
+
+    if(!title)
+        if(hindi[0] === '>')
+            hindi = hindi.substring(1).trim();
+        else
+            title = hindi;
+    if(title) {
+        cleanupEntry();
+        entry = {
+            szo: title,
+            szarm,
+            ford: []
+        };
+        if(alt)
+            entry.alt = alt.split(commaSplitter);
+        entriesFile.szavak.push(entry);
+    }
+    let nytSzarm;
+    [nyt, nytSzarm] = parseGrammar(nyt);
+    if(nytSzarm)
+        entry.szarm = entry.szarm ? `${entry.szarm} ${nytSzarm}` : nytSzarm;
+
+    let p = 0,
+        word = '',
+        inParen = false,
+        sec: number = Section.Grammar,
+        ford = newFord();
+    if(!title)
+        ford.kif = hindi;
+    ford.nyt = nyt;
+    while(true) {
+        const end = p === hun.length;
+        if(!end && !boundary.includes(hun[p])) {
+            word += hun[p++];
+            continue;
+        }
+        const sep = end ? '' : hun[p];
+        if(word) {
+            if(sec === Section.Grammar) {
+                if(hunGram.includes(word)) {
+                    if(ford.nyt)
+                        ford.nyt += ' ';
+                    ford.nyt += word;
+                } else
+                    sec = Section.Meaning;
+            }
+            if(sec === Section.Meaning) {
+                if(end || !inParen && (sep === ',' || sep === ';')) {
+                    const rexRes = meaningWithComment.exec(word);
+                    if(rexRes) {
+                        ford.ert.push({
+                            szo: rexRes[1].trim(),
+                            megj: rexRes[2].trim()
+                        });
+                    } else
+                        ford.ert.push(word);
+                    if(sep === ';') {
+                        entry.ford.push(ford);
+                        ford = newFord();
+                        sec = Section.Grammar;
+                    }
+                } else {
+                    word += sep;
+                    p++;
+                    if(sep === '(')
+                        inParen = true;
+                    else if(sep === ')')
+                        inParen = false;
+                    continue;
+                }
+            }
+            word = '';
+        } else if(sep === '(') {
+            word += sep;
+            inParen = true;
+        }
+        if(end)
+            break;
+        p++;
+    }
+    entry.ford.push(ford);
+}
+
+function parseGrammar(nyt: string) {
+    if(!nyt)
+        return [null, null];
+    const pt = nyt.split(nonAlphaSplitter);
+    const nytArr: string[] = [];
+    const szarmArr: string[] = [];
+    for(const p of pt) {
+        if(!p)
+            continue;
+        if(uppercasedWord.test(p))
+            szarmArr.push(p);
+        else {
+            const conv = engGram[p];
+            if(conv !== '')
+                nytArr.push(conv ? conv : p);
+        }
+    }
+    return [nytArr.join(' '), szarmArr.join(' ')];
 }
 
 function newFord() {
